5fd80ffec262d602e18123ecc9edc531858ba5b2
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-07-04 04:28:29 +0700

    Fix for optimized VWpol computing
    Stupid but major bug in Wpol2VWpol_opt

--- private/Wpol2VWpol_opt.m
+++ private/Wpol2VWpol_opt.m
@@ -2,12 +2,12 @@ function [ VWpol ] = Wpol2VWpol_opt( rel, Wpol )
 %Wpol2VWpol_opt Apply rel to Wpol but without excessive zero padding
 %   Detailed explanation goes here
 
-if nnz(rel)
+if nnz(~rel)
     for i = numel(rel):-1:1
         if rel(i)
             VWpol(i,:) = [0 Wpol(i,:)];
         else
-            VWpol(i,:) = [Wpol(i,:),0];
+            VWpol(i,:) = [Wpol(i,:) 0];
         end
     end
 else
