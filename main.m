f3d5ad4406d1ca672437d3553d883addeca9dd27
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-07-04 17:55:41 +0700

    Updated stats and changed counters' output order

--- main.m
+++ main.m
@@ -20,10 +20,10 @@ cnt.MULTICOMP = 0;
 cnt.NUMEL2    = 0;
 cnt.NUMEL3    = 0;
 cnt.NUMEL4    = 0;
+cnt.MAXDEG    = 0;
 cnt.FULL      = 0;
 cnt.CHAIN     = 0;
 cnt.CYCLE     = 0;
-cnt.MAXDEG    = 0;
 cnt.TREE      = 0;
 cnt.HNODES    = 0;
 cnt.CHAINRED  = 0;
@@ -70,33 +70,34 @@ disp('---------------------------------------------');
 %arpa_num = 21;                          %~0.9s             %2012-03-07
 %arpa_num = 22;                          %~0.9s             %2012-03-07
     arpa_time(21:22)=[ 1 1 ];                               %2012-03-07
-%arpa_num = 23;                          %~2s               %2012-03-12
-%arpa_num = 31;                          %~2s               %2012-03-12
-    arpa_time(23:31)=[ 2 2 2 2 2 2 2 2 2 ];                 %2012-03-12
+%arpa_num = 23;                          %~1s               %2012-07-04
+%arpa_num = 31;                          %~1s               %2012-07-04
+    arpa_time(23:31)=[ 1 1 1 1 1 1 1 1 1 ];                 %2012-07-04
 %arpa_num = 36;                          %~5s               %2012-07-04
     arpa_time(32:36)=[ 5 5 5 5 5 ];                         %2012-07-04
-%arpa_num = 37;                          %~25s              %2012-07-04
+%arpa_num = 37;                          %~23s              %2012-07-04
 %arpa_num = 38;                          %
 %arpa_num = 39;                          %
-    arpa_time(37:39)=[ 25 25 25];                           %2012-07-04
-%arpa_num = 42;                          %~105s  1m45s      %2012-07-04
+    arpa_time(37:39)=[ 23 23 23];                           %2012-07-04
+%arpa_num = 42;                          %~98s   1m38s      %2012-07-04
 %arpa_num = 44;                          %
 %arpa_num = 45;                          %
 %arpa_num = 46;                          %
 %arpa_num = 47;                          %
 %arpa_num = 48;                          %
-    arpa_time(40:48)=[ 105 105 105   0   0   0   0   0   0 ];%2012-07-04
+    arpa_time(40:48)=[ 98 98 98  0  0  0  0  0  0 ];        %2012-07-04
 %arpa_num = 49;                          %
 %%arpa_num = 50;                          % should be equal to 49
-%arpa_num = 51;                          %~902s 15m02s       %2012-03-12
-%arpa_num = 51;                          %~758s 12m38s       %2012-03-13
-arpa_num = 51;                          %~722s 12m02s       %2012-07-04
+%arpa_num = 51;                          %~902s 15m02s      %2012-03-12
+%arpa_num = 51;                          %~758s 12m38s      %2012-03-13
+%arpa_num = 51;                          %~722s 12m02s      %2012-07-04
     arpa_time(49:51)=[   0   0  902];
 %arpa_num = 52;                          %
 %arpa_num = 53;                          %
 %arpa_num = 54;                          %
 %arpa_num = 55;                          %
-%arpa_num = 56;                          %~4617s ~1h17m      %2012-03-15
+%arpa_num = 56;                          %~4617s ~1h17m     %2012-03-15
+%arpa_num = 56;                          %~4144s ~1h10m     %2012-07-04
     arpa_time(52:56)=[    0    0    0    0 4617 ];          %2012-03-15
 %arpa_num = 57;                          %
     arpa_time(57)=     0;
@@ -144,8 +145,8 @@ ExP_coeffs = ExP_coeffs(find(ExP_coeffs,1):end); % fails if all ExP_coeffs are z
 
 
 %fprintf('\nECP result:   %s\n',char(ExP_res));
-fprintf('\nno-edges:  %i\nmulticomp: %i\nnumel2:    %i\nnumel3:    %i\nnumel4:    %i\nfull5+:    %i\nchain5+:   %i\ncycle5+:   %i\nmaxdeg:    %i\ntree5+:    %i\nnhodes:    %i\nchainred:  %i\nreliable:  %i\nbranch:    %i\n----------------\nTOTAL:     %i\n', ...
-        [  cnt.NOEDGES    cnt.MULTICOMP  cnt.NUMEL2     cnt.NUMEL3     cnt.NUMEL4     cnt.FULL       cnt.CHAIN      cnt.CYCLE      cnt.MAXDEG     cnt.TREE       cnt.HNODES     cnt.CHAINRED   cnt.RELIABLE   cnt.BRANCHING                    cnt.TOTAL          ]);
+fprintf('\nno-edges:  %i\nmulticomp: %i\nnumel2:    %i\nnumel3:    %i\nnumel4:    %i\nmaxdeg:    %i\nfull5+:    %i\nchain5+:   %i\ncycle5+:   %i\ntree5+:    %i\nnhodes:    %i\nchainred:  %i\nreliable:  %i\nbranch:    %i\n----------------\nTOTAL:     %i\n', ...
+        [  cnt.NOEDGES    cnt.MULTICOMP  cnt.NUMEL2     cnt.NUMEL3     cnt.NUMEL4     cnt.MAXDEG     cnt.FULL       cnt.CHAIN      cnt.CYCLE      cnt.TREE       cnt.HNODES     cnt.CHAINRED   cnt.RELIABLE   cnt.BRANCHING                    cnt.TOTAL          ]);
 %fprintf('\nHIT:  %i\nMISS: %i\nHIT/MISS ratio: %f', [HIT MISS HIT/MISS]);
 fprintf('\nElapsed time is %8.6f seconds\n\n',tElapsed);
 fprintf('\nECP coeffs:   %s\n',int2str(ExP_coeffs));
