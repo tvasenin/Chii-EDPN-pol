645c808a2caaacbfa370f8674be9b2c7bcd9db8f
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-07-07 06:27:25 +0700

    Critical typo in numel2_v2
    Case 'both nodes are unreliable' was fully broken!

--- ECPN_C_numel2_pol_v2.m
+++ ECPN_C_numel2_pol_v2.m
@@ -2,16 +2,17 @@ function P = ECPN_C_numel2_pol_v2(rel,Wpol)
 %ECPN_C_numel2 Calculates ECPN for connected graph, where n = 2;
 %   Detailed explanation goes here
 
+% P has various resulting size in these four cases
 if rel(1)
-    if rel(2)
+    if rel(2) % [1 1]
         P = conv2(Wpol(1,:),Wpol(2,:));
-    else
+    else      % [1 0]
         P = conv2(Wpol(1,:),[Wpol(2,:) 0]);
     end
-elseif rel(2)
+elseif rel(2) % [0 1]
     P = conv2([Wpol(1,:) 0],Wpol(2,:));
-else
-    P = [conv2(Wpol(1,:),Wpol(2,:)) 0];
+else          % [0 0]
+    P = [conv2(Wpol(1,:),Wpol(2,:)) 0 0];
 end
 
 end
