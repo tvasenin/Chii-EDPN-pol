c271cfa2829407479202b71be1178d784cd93c7b
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-03-10 21:11:15 +0700

    In-place poly_add
    small speed improvement

--- private/poly_add.m
+++ private/poly_add.m
@@ -1,9 +1,9 @@
-function [ c ] = poly_add( a, b )
+function [ a ] = poly_add( a, b )
 %poly_add Adds 2 polynoms
 %   Detailed explanation goes here
 
 k = length(b) - length(a);
-c = [zeros(1,k) a] + [zeros(1,-k) b];
+a = [zeros(1,k) a] + [zeros(1,-k) b];
 
 end
 
