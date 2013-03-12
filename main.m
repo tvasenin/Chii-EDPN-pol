60089705bc2de4f25da1813e52458c1d0014c4e8
Timofey Vasenin <timofey.vasenin@gmail.com>
2013-03-13 01:18:25 +0700

    Fix remaining old gen_arpa call

--- main.m
+++ main.m
@@ -122,5 +122,5 @@ fprintf('\nno-edges:  %i\nmulticomp: %i\nnumel2:    %i\nnumel3:    %i\nnumel4:  
 fprintf('\nElapsed time is %8.6f seconds\n\n',tElapsed);
 fprintf('\nECP coeffs:   %s\n',int2str(ExP_coeffs));
 
-[~, ~, ~] = gen_arpanet_small(arpa_num);
+[~, ~, ~] = gen_arpanet(arpa_num);
 if isequal(ref, ExP_coeffs') || isequal(ref, ExP_coeffs), disp('OK!'); else disp('FAILED!'); end
